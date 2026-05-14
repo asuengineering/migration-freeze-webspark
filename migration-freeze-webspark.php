fd0557a8961cf3af1b5e8654ae1c9180b73adb5e
Steve Ryan <steve.ryan@asu.edu>
2026-05-14 16:08:10 -0700

    Add migration audit trail export framework and history

--- migration-freeze-webspark.php
+++ migration-freeze-webspark.php
@@ -7,7 +7,7 @@
  * Author URI:      https://comm.engineering.asu.edu
  * Text Domain:     migration-freeze-webspark
  * Domain Path:     /languages
- * Version:         0.4.0
+ * Version:         0.5.0
  *
  * @package         migration_freeze_webspark
  *
@@ -19,7 +19,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 if ( ! defined( 'MFW_VERSION' ) ) {
-	define( 'MFW_VERSION', '0.4.0' );
+	define( 'MFW_VERSION', '0.5.0' );
 }
 
 if ( ! defined( 'MFW_PLUGIN_FILE' ) ) {
@@ -68,18 +68,13 @@ require_once MFW_PLUGIN_PATH . 'inc/admin-notices.php';
 require_once MFW_PLUGIN_PATH . 'inc/my-sites.php';
 require_once MFW_PLUGIN_PATH . 'inc/user-management.php';
 require_once MFW_PLUGIN_PATH . 'inc/settings.php';
+require_once MFW_PLUGIN_PATH . 'inc/audit-trail.php';
 
-/**
- * Load translations.
- */
 function mfw_bootstrap() {
 	load_plugin_textdomain( 'migration-freeze-webspark', false, dirname( MFW_PLUGIN_BASENAME ) . '/languages' );
 }
 add_action( 'plugins_loaded', 'mfw_bootstrap' );
 
-/**
- * Plugin activation hook.
- */
 function mfw_activate_plugin() {
 	if ( false === get_option( MFW_OPTION_SITE_STATE, false ) ) {
 		add_option( MFW_OPTION_SITE_STATE, MFW_STATE_PENDING );
@@ -87,10 +82,6 @@ function mfw_activate_plugin() {
 }
 register_activation_hook( __FILE__, 'mfw_activate_plugin' );
 
-/**
- * Plugin deactivation hook.
- */
 function mfw_deactivate_plugin() {
-	// No-op for now.
 }
 register_deactivation_hook( __FILE__, 'mfw_deactivate_plugin' );
